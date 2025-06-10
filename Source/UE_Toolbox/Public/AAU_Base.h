55fbf43c4a9a7987302407be68b3ad7dbf5244e0
Denis Dubin <37864505+SpaceOak@users.noreply.github.com>
2025-06-10 12:41:51 +0200

    Public. Init parent ui classes

---
+++ Source/UE_Toolbox/Public/AAU_Base.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AssetActionUtility.h"
+#include "AAU_Base.generated.h"
+
+/**B
+ * 
+ */
+UCLASS()
+class UE_TOOLBOX_API UAAU_Base : public UAssetActionUtility
+{
+	GENERATED_BODY()
+	
+};
